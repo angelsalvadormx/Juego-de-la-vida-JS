8750cbbefe4b0890ec0bc2a32ffd988010a6f8b2
Angel Salvador <02angelsalvador@gmail.com>
2019-11-30 13:47:45 -0600

    mostrar los recuadros en el main desde js

--- js/juego_de_la_vida.js
+++ js/juego_de_la_vida.js
@@ -1,11 +1,28 @@
-const datos = {
-  "activos" : ["1,1","2,1","3,1"]
+// Configuracion inicial
+const conf = {
+  "vivos" : ["1,1","2,1","3,1"],
+  "width" : 5,
+  "height" : 5,
+  "tablero" : document.getElementById('main')
 }
 
 // iniciar el juego
 const start = ()=>{
+  cargarTablero();
+}
 
+const cargarTablero = () => {
+  let html = "";
+  for (let i = 0; i < conf.width; i++) {
+    for (let j = 0; j < conf.height; j++) {
+      html += "<i id='"+i+"-"+j+"'></i>";
+    }
+  }
+  conf.tablero.innerHTML = html;
 }
 
 // actualizar generacion
-const 
+const buscarSobrevientes = (vivos)=>{
+
+}
+
