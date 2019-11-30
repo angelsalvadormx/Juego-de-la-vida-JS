2b71af781853bc11b9fce8aeb1ba02df8f610617
Angel Salvador <02angelsalvador@gmail.com>
2019-11-30 14:17:12 -0600

    [nuevo] funcion 'buscar vecinos'

--- js/juego_de_la_vida.js
+++ js/juego_de_la_vida.js
@@ -1,30 +1,53 @@
 // Configuracion inicial
 const conf = {
-  "vivos" : ["1,1","2,1","3,1"],
-  "width" : 5,
-  "height" : 5,
-  "tablero" : document.getElementById('main')
+  "vivos": ["1,1", "2,1", "3,1"],
+  "width": 5,
+  "height": 5,
+  "tablero": document.getElementById('main')
 }
 
 // iniciar el juego
-const start = ()=>{
+const start = () => {
   cargarTablero();
+  buscarSobrevivientes()
+  /*setInterval(() => {
+  }, 2000);*/
 }
 
 const cargarTablero = () => {
-  let html = "",clase = "", found = undefined;
+  let html = "", clase = "", found = undefined;
   for (let i = 0; i < conf.width; i++) {
     for (let j = 0; j < conf.height; j++) {
-      found = conf.vivos.find(vivo=> vivo == i+","+j);
-      clase = found != undefined ? 'vivo':'' 
-      html += "<i id='"+i+","+j+"' class='"+clase+"'></i>";
+      found = conf.vivos.find(vivo => vivo == i + "," + j);
+      clase = found != undefined ? 'vivo' : ''
+      html += "<i id='" + i + "," + j + "' class='" + clase + "'></i>";
     }
   }
   conf.tablero.innerHTML = html;
 }
 
 // actualizar generacion
-const buscarSobrevientes = (vivos)=>{
-
+const buscarSobrevivientes = () => {
+  console.log('Buscando sobrevivientes');
+  let vecinos = buscarVecinos(1,2);
+  console.log(vecinos);
+  
 }
+
+const buscarVecinos = (x,y) => {
+  let vecinos = [];
+  let posicion = x+','+y;
+  let tmpY = 0;
+  x++;
+  for (let i = 0; i < 3; i++) {
+    tmpY = y + 1;
+    for (let j = 0; j < 3; j++) {
+      vecinos.push(x+","+tmpY);
+      tmpY--;
+    }
+    x--;
+  }
+  return vecinos.filter(vecino => vecino != posicion );  
+}
+
 
